192cea8911d01fe22b977d9e91aa04aa5119805e
Shihaam <sshafanashihaam@gmail.com>
2025-07-29 21:05:27 +0530

    Week 6 Solutions Uploaded

---
+++ Week6/myfirstreact/src/App.js
@@ -0,0 +1,5 @@
+function App() {
+  return <h1>Welcome to the first session of REACT!!</h1>;
+}
+
+export default App;
---
+++ Week6/student_app/src/App.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
+
+function App() {
+  return (
+    <Router>
+      <div style={{ padding: "20px" }}>
+        <h1>StudentApp</h1>
+        <nav>
+          <ul
+            style={{
+              display: "flex",
+              gap: "20px",
+              listStyleType: "none",
+              paddingLeft: 0,
+            }}
+          >
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+        </nav>
+        <hr />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
